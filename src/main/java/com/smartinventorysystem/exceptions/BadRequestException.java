a45a576f23e3dde453b58f6abdcce3cc28071399
sampanna17 <sampannapiya13@gmail.com>
2026-07-28 14:43:10 +0545

    Exception Correection

--- src/main/java/com/smartinventorysystem/exceptions/BadRequestException.java
+++ src/main/java/com/smartinventorysystem/exceptions/BadRequestException.java
@@ -1,6 +1,6 @@
 package com.smartinventorysystem.exceptions;
 
-public class BadRequestException extends RuntimeException {
+public class  BadRequestException extends RuntimeException {
     public BadRequestException(String message) {
         super(message);
     }
